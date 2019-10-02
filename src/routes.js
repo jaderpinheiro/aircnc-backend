149be8c7fc8e47c1129be02869c030d1edfada4f
jaderpinheiro <jaderbp@gmail.com>
2019-10-01 23:45:27 -0300

    Criando Rotas, Server, Session User

---
+++ src/routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const SessionController = require('./controllers/SessionController');
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.store);
+module.exports = routes;
