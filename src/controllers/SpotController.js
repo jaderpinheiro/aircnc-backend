1dc45fe986c1d3b419061c7b0d1c22d9b05cb5f2
jaderpinheiro <jaderbp@gmail.com>
2019-10-02 00:35:14 -0300

    rota  listagem tech

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -1,7 +1,15 @@
 const User = require('../models/User');
 const Spot = require( '../models/Spot');
+
 module.exports ={
-    async store( req, res){
+    async index(req, res){
+        const { tech }= req.query;
+
+        const spots = await Spot.find({techs: tech});
+
+        return res.json(spots);
+    },
+    async store( req, res){      
         const { filename } = req.file;
         const { company, techs, price } = req.body;
         const { user_id } = req.headers;
--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,6 @@ const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
 
+routes.get('/spots',SpotController.index);
 routes.post('/spots', upload.single('thumbnail'),SpotController.store);
 module.exports = routes;
